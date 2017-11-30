3ad20c0c2ef9dcd30b413597875823e97c6c1514
noyon <noyon@DESKTOP-HBF8BPB>
2017-11-30 17:38:58 +0600

    Initial project

---
+++ OnlineShoppingStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EfDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OnlineShoppingStore.Domain/Concrete/EFProductRepossitory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EfProductRepossitory : IProductRepository
+    {
+        private readonly EfDbContext _context=new EfDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return _context.Products; }
+            
+        }
+    }
+}
